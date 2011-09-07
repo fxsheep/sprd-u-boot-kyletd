659662e7d59bceb3cc355948043ec5fdc714dab4
markyang <mark.yang@spreadtrum.com>
2011-09-07 21:40:14 +0800

    open calibration detect timeout
    Change-Id: I232927da25e55990d49ce7ba667e73647b359449

--- common/idh_layout.c
+++ common/idh_layout.c
@@ -3,3 +3,8 @@ char * get_mtdparts(void)
 {
         return MTDPARTS_DEFAULT;
 }
+
+int get_cal_det_ms(void)
+{
+    return CALIBERATE_DETECT_MS;
+}
--- include/configs/sp6810a.h
+++ include/configs/sp6810a.h
@@ -293,5 +293,7 @@
 #endif
 #endif // CONFIG_LCD
 
+#define CALIBERATE_DETECT_MS 20000
+
 
 #endif /* __CONFIG_H */
