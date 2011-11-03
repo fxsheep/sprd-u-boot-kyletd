fa44137c1cd6065a043e47268dff7bd13c28aa3b
markyang <mark.yang@spreadtrum.com>
2011-11-03 21:24:35 +0800

    forget modify some headerfiles
    Change-Id: Ia31a74b3e453f33e7446de55e867a9963397a800

--- include/configs/sp8805ga.h
+++ include/configs/sp8805ga.h
@@ -298,5 +298,6 @@
 
 #define CALIBERATE_DETECT_MS 30000
 #define LOW_BAT_ADC_LEVEL 0x330 /*phone battery adc value low than this value will not boot up*/
+#define PWR_KEY_DETECT_CNT 12 /*this should match the count of boot_pwr_check() function */
 
 #endif /* __CONFIG_H */
