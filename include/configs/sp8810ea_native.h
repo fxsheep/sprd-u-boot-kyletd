53eda670d2f07c362abd007671d6f9addd4a0601
Chen Gaopeng <roc.chen@spreadtrum.com>
2012-05-15 14:49:42 +0800

    add ttyS1 as android console to turn on job control
    Change-Id: I97a4cc9f75006a0382e3a815df8a674491f91da3

--- include/configs/sp8810ea_native.h
+++ include/configs/sp8810ea_native.h
@@ -237,7 +237,7 @@
 //#define MTDPARTS_DEFAULT "mtdparts=sprd-nand:256k(spl),512k(2ndbl),128k(params),512k(vmjaluna),10m(modem),10m(boot),5120k(dsp),1280k(fixnv),3840k(backupfixnv),3840k(runtimenv),10m(recovery),150m(system),300m(userdata),1m(boot_logo),1m(fastboot_logo),2m(cache),256k(misc)"
 //#define MTDPARTS_DEFAULT "mtdparts=sprd-nand:256k(spl),512k(2ndbl),128k(params),512k(vmjaluna),10m(modem),10m(boot)"
 #define MTDPARTS_DEFAULT "mtdparts=sprd-nand:256k(spl),512k(2ndbl),256k(params),512k(vmjaluna),10m(modem),3840k(fixnv),3840k(backupfixnv),5120k(dsp),3840k(runtimenv),10m(boot),10m(recovery),200m(system),230m(userdata),20m(cache),256k(misc),1m(boot_logo),1m(fastboot_logo),3840k(productinfo),512k(kpanic)"
-#define CONFIG_BOOTARGS "mem=240M console=ttyS1,115200n8 init=/init " MTDPARTS_DEFAULT
+#define CONFIG_BOOTARGS "mem=240M console=ttyS1,115200n8 androidboot.console=ttyS1 init=/init " MTDPARTS_DEFAULT
 #endif
 
 #define COPY_LINUX_KERNEL_SIZE	(0x600000)
