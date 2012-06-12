7487ca0a7832c4b0cdc63c9446dfd97f70402ccd
xiaobo <xiaobo@spreadst.com>
2012-06-12 17:12:05 +0800

    change ALARM_LEAD_SET_MS to 0 for ics.
    Change-Id: Id01186ce6684999748f50cb4b089104bb2059f71

--- include/configs/amazing.h
+++ include/configs/amazing.h
@@ -311,6 +311,6 @@
 
 #define LOW_BAT_ADC_LEVEL 782 /*phone battery adc value low than this value will not boot up*/
 #define PWR_KEY_DETECT_CNT 12 /*this should match the count of boot_pwr_check() function */
-#define ALARM_LEAD_SET_MS 60 /* time set for alarm boot in advancd */
+#define ALARM_LEAD_SET_MS 0 /* time set for alarm boot in advancd */
 
 #endif /* __CONFIG_H */
