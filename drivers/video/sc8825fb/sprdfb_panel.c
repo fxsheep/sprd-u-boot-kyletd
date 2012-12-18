939c85e005941988a465107bd35b2884420fa7b9
jessica.lu <jessica.lu@spreadtrum.com>
2012-12-18 11:10:57 -0500

    Bug#72384 Follow LCM vendor's suggestion, do panel reset after mipi bus enter lp11.
    [bug number  ]Bug#72384
    [root cause  ]no
    [changes     ]Follow LCM vendor's suggestion, do panel reset after mipi bus enter lp11.
    [side effects]no
    [reviewers   ]hansen.sun
    
    Change-Id: Ibc2c1e1fc89b2eeda4ca29263c1daac9fc00bb4a

--- drivers/video/sc8825fb/sprdfb_panel.c
+++ drivers/video/sc8825fb/sprdfb_panel.c
@@ -280,8 +280,8 @@ static struct panel_spec *adapt_panel_from_readid(struct sprdfb_device *dev)
 	for(i = 0;i<(sizeof(lcd_panel))/(sizeof(lcd_panel[0]));i++) {
 		FB_PRINT("sprdfb: [%s]: try panel 0x%x\n", __FUNCTION__, lcd_panel[i].lcd_id);
 		panel_mount(dev, lcd_panel[i].panel);
-		panel_reset(lcd_panel[i].panel);
 		panel_init(dev);
+		panel_reset(lcd_panel[i].panel);
 		id = dev->panel->ops->panel_readid(dev->panel);
 		if(id == lcd_panel[i].lcd_id) {
 			FB_PRINT("sprdfb: [%s]: LCD Panel 0x%x is attached!\n", __FUNCTION__, lcd_panel[i].lcd_id);
