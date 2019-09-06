868b3bceac17bc5cfed80dfb95ea690fa37eb098
Sheep <sunxiaoyang2003@gmail.com>
2019-09-06 22:59:39 +0800

    Revert "Fix sdram"
    This reverts commit dbb00ac22c8b47bfc8eeafb5653b9e7b51a9a16c.

--- arch/arm/include/asm/arch-sc8810/sdram_cfg.h
+++ arch/arm/include/asm/arch-sc8810/sdram_cfg.h
@@ -16,26 +16,4 @@
 
 #include "sci_types.h"
 #include "sdram_drvapi.h"
-
-struct sc8810_ddr_reset_para {
-	char id[5];
-
-	// clock
-	u32 emc_clk;
-
-	// driver strength
-	u8 dqs_drv;
-	u8 dat_drv;
-	u8 ctl_drv;
-	u8 clk_drv;
-
-	// clk wr
-	u8 clk_wr;
-	u16 read_value;  // value from 0x20000174
-};
-
-struct sc8810_ddr_reset_data {
-struct sc8810_ddr_reset_para *ddr_para;
-int 						 size;
-};
 #endif /* SDRAM_CFG_H */
